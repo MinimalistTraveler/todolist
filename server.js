a6d83306dedfe956cb65281176a1d8fc33e58d17
Erik <erikzb7@gmail.com>
2018-12-08 00:18:21 -0500

    server.js bug fix attempt

--- server.js
+++ server.js
@@ -4,18 +4,21 @@ require("@babel/polyfill");
 
 var _restify = _interopRequireDefault(require("restify"));
 
-var _routes = _interopRequireDefault(require("./server/routes/routes"));
+var _routes = _interopRequireDefault(require("./routes/routes"));
 
-var _restifyCorsMiddleware = _interopRequireDefault(require("restify-cors-middleware"));
+var _restifyCorsMiddleware = _interopRequireDefault(
+  require("restify-cors-middleware")
+);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 var PORT = process.env.PORT || 5000;
 
 var server = _restify.default.createServer({
   name: "Todolist App"
 }); // CORS
-
 
 var cors = (0, _restifyCorsMiddleware.default)({
   preflightMaxAge: 5,
@@ -28,10 +31,9 @@ server.use(cors.actual); // API ROUTER
 
 _routes.default.applyRoutes(server, "/api"); // Plugins
 
-
 server.use(_restify.default.plugins.bodyParser());
 server.use(_restify.default.plugins.queryParser()); // Listening
 
-server.listen(PORT, function () {
+server.listen(PORT, function() {
   return console.log("Listening on port ".concat(PORT));
 });
